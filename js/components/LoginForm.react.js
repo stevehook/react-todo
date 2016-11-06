f78fdb976851663dd259d70f6e07b4d372c6b677
Steve Hook <steve.hook@gmail.com>
2016-11-06 19:26:28 +0000

    Tidy up login form and add default value (just for testing)

--- js/components/LoginForm.react.js
+++ js/components/LoginForm.react.js
@@ -7,12 +7,16 @@ const LoginForm = React.createClass({
     return {};
   },
 
+  componentDidMount: function(){
+    this.refs.loginEmail.focus();
+  },
+
   render: function() {
     return (
       <div className="form-container">
         <form onSubmit={this.handleSubmit}>
-          <input type='text' className='form-control user-email' ref='loginEmail' placeholder='Enter your email address' autofocus/>
-          <input type='pasword' className='form-control user-password' ref='loginPassword'/>
+          <input type='text' className='form-control user-email' ref='loginEmail' placeholder='Enter your email address' defaultValue='bob@example.com'/>
+          <input type='pasword' className='form-control user-password' ref='loginPassword' defaultValue='secret'/>
           <button type='submit' className='btn btn-default'>Login</button>
         </form>
       </div>
